a2f3b982a479ed152be2142ace1bd82602577ca4
Evgeniy Kostromin <ewg.kostromin@yandexru>
2023-05-19 14:35:24 +0500

    Задача 32

--- task_32.py
+++ task_32.py
@@ -7,10 +7,25 @@
 # Output:
 # 2(7), 4(5), 5(8)
 
-list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
-min_number = int(input('Минимальное значение: '))
-max_number = int(input('Максимальное значение: '))
-for i in range(len(list_1)):
-  if min_number <= list_1[i] <= max_number:
-    print(f'индексы элементов в массиве: {i}')
+arr = [1, 3, 7, 10, 5, 8]
+min_val = int(input('Минимальное значение: '))
+max_val = int(input('Максимальное значение: '))
+indexes = []
+for i in range(len(arr)):
+    if arr[i] >= min_val and arr[i] <= max_val:
+        indexes.append(i)
+
+print(f'индексы элементов в массиве: {indexes}')
+
+# решение через генератор
+result = [i for i in range(len(arr)) if arr[i] >= min_val and arr[i] <= max_val]
+print(result)
+
+# эталонное решение
+# list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+# min_number = int(input('Минимальное значение: '))
+# max_number = int(input('Максимальное значение: '))
+# for i in range(len(list_1)):
+#   if min_number <= list_1[i] <= max_number:
+#     print(f'индексы элементов в массиве: {i}')
 
